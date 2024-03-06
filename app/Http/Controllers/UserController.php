04beaf15244d671e3b60e543bd6172c8e40dd394
firstiaaa <firstiaazh@gmail.com>
2024-03-06 10:24:14 +0700

    praktikum 2.4 jb 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = UserModel::all();
-        $jumlahPengguna = UserModel::count(); // Menghitung jumlah total pengguna
-        return view('user', ['users' => $users, 'jumlahPengguna' => $jumlahPengguna]);
+      $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager33',
+            'nama' => 'Manager Tiga Tiga',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
+        ],
+    );
+    $user->save();
+    
+    return view ('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,29 +5,21 @@
 </head>
 <body>
     <h1>Data User</h1>
-    {{-- <table border="1" cellpadding="2" cellspacing="0">
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($users as $user)
+        @foreach ($data as $d)
             <tr>
-                <td>{{ $user->user_id }}</td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->nama }}</td>
-                <td>{{ $user->level_id }}</td>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
             </tr>
-        @endforeach
-    </table> --}}
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $users->count() }}</td>
-        </tr>
+            @endforeach
     </table>
 </body>
 </html>
