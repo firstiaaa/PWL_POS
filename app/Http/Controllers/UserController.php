fb94bdeea45932285a1fd8e0658d883a4452fefb
firstiaaa <firstiaazh@gmail.com>
2024-03-06 09:10:53 +0700

    praktikum 2.2 jb 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,11 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-
-//    $user = UserModel::find(1);
-//     $user = UserModel::firstWhere('level_id', 1);
-        $user = UserModel::findOr(20, ['username','nama'], function(){
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
