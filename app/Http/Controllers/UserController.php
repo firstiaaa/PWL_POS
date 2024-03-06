8218af1a7f0e5513d08fa02449b9a4ca5d66b86b
firstiaaa <firstiaazh@gmail.com>
2024-03-06 09:04:44 +0700

    praktikum 2.1 jb 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,26 +11,10 @@ class UserController extends Controller
     public function index()
     {
 
-    // tambah data user dengan Eloquent Model 
-    // $data = [ 
-    //     'username' => 'customer-1',
-    //     'nama' => 'Pelanggan',
-    //     'password' => Hash::make('12345'),
-    //     'level_id' => 5
-    // ];
-    // UserModel::insert($data); // update data user
-
-    $data = [ 
-        'level_id' => 2,
-        'username' => 'manager_tiga',
-        'nama' => 'Manager 3',
-        'password' => Hash::make('12345')
-    ];
-    UserModel::create($data); // update data user
-    
-    
-    // coba akses model UserModel
-    $user = UserModel::all(); // ambil semua data dari tabel m_user 
-    return view('user', ['data' => $user]);
+//    $user = UserModel::find(1);
+//     $user = UserModel::firstWhere('level_id', 1);
+        $user = UserModel::findOr(20, ['username','nama'], function(){
+        });
+        return view('user', ['data' => $user]);
     }
 }
