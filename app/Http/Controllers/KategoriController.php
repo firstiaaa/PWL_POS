0da29e87ab79480feec09b3097ff5c1f60ddd553
firstiaaa <firstiaazh@gmail.com>
2024-03-13 12:07:53 +0700

    praktikum 1 2 jb 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,29 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
 
 class KategoriController extends Controller
 {
-    public function index()
+    // public function index()
+    // {
+    //     $data = [
+    //         'kategori_kode' => 'SNK',
+    //         'kategori_nama' => 'Snack/Makanan Ringan',
+    //         // 'created_at' => now()
+    //     ];
+    //     // DB::table('m_kategori')->insert($data);
+    //     // return 'Insert data baru berhasil';
+
+    //     // $row = DB::table('m_kategori')->where('kategori_kode','SNK')->update(['kategori_nama' => 'ATK']);
+    //     // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris ';
+
+    //     $row = DB::table('m_kategori')->where('kategori_kode','SNK')->delete();
+    //     return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris ';
+
+    //     // $data = DB::table('m_kategori')->get();
+    //     // return view ('kategori', ['data' => $data]);
+    // }
+
+    public function index (KategoriDataTable $dataTable)
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            // 'created_at' => now()
-        ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-
-        // $row = DB::table('m_kategori')->where('kategori_kode','SNK')->update(['kategori_nama' => 'ATK']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris ';
-
-        $row = DB::table('m_kategori')->where('kategori_kode','SNK')->delete();
-        return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris ';
-
-        // $data = DB::table('m_kategori')->get();
-        // return view ('kategori', ['data' => $data]);
+        return $dataTable->render('kategori.index');
     }
 
+    public function create()
+    {
+        return view('kategori.create');
+    }
+    public function store (Request $request)
+    {
+        KategoriModel::create ([
+        'kategori_kode' => $request->kodekategori, 
+        'kategori_nama' => $request->namakategori,
+        ]);
+            return redirect('/kategori');
+    }
 }
