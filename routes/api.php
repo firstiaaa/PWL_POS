ce8b4bb754541769d4bcc90a2a889458f7a31c83
firstiaaa <firstiaazh@gmail.com>
2024-04-30 20:22:38 +0700

    js 10 b1p4

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,3 +30,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 // logout
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+// CRUD LEVEL
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
