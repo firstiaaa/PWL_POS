d3d4c3b2947b4ba0fd0f7d710ed7c4d3aeb26c73
firstiaaa <firstiaazh@gmail.com>
2024-05-08 10:32:13 +0700

    praktikum 1 js 11

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,22 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Model; use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
-    public function getJWTIdentifier(){
-        return 'user_id';
-    }    
 
-    public function getJWTCustomClaims()
-    {
-        return[];
-    }
+public function getJWTIdentifier(){ return $this->getKey();
+}
 
-    protected $table ='m_user';
-    protected $primaryKey ='user_id';
-    protected $fillable =['level_id','username','nama','password'];
+public function getJWTCustomClaims(){ return [];
+}
+
+protected $table = 'm_user'; protected $primaryKey = 'user_id';
+
+protected $fillable = [ 'username', 'nama', 'password', 'level_id', 'image'//tambahan
+];
+
+public function level()
+{
+return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+}
+protected function image(): Attribute
+{
+return Attribute::make(
+get: fn ($image) => url('/storage/posts/' . $image),
+);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -61,3 +61,6 @@ Route::post('barang', [BarangController::class, 'store']);
 Route::get('barang/{barang}', [BarangController::class, 'show']);
 Route::put('barang/{barang}', [BarangController::class, 'update']);
 Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+
+//js 11
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
